1d07380ea96e5d6a5af7eafcec0373e0c063da24
Ghaidaa Saeed <ghaidaa.s17@gmail.com>
2019-04-14 22:12:45 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@
 let card = document.getElementsByClassName("card");
 let cards=[...card];
 let deck = document.querySelector(".deck");
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -39,56 +38,64 @@ function startGame(){
   var matched = 0;
   var cardSelected=false;
   var starsNum =3;
+  document.getElementById("moves").textContent=moveCount;
   $(".card").click(function(){
-    start();
     moveCount+=1;
+    if (moveCount===1) {
+      start();
+    }
     document.getElementById("moves").textContent=moveCount;
-    if((cardSelected==true) && (matched<=8) ){
-      var curCard= $(this);
-      var curCardType=curCard.children().attr("class");
 
-    if (curCardType === preCardType) {
-      curCard.toggleClass("match show open");
-      preCard.toggleClass("match show open");
-      cardSelected=false;
-      matched+=1;
-    if(matched==8){
-      clearInterval(intervalId);
-      swal({
-        title: "Congratulations! You Won!",
-        text: "With "+moveCount+" moves and "+starsNum+" stars woooo!"  ,
-        type: "success",
-        button: "play again",
-          }).then(function() {
-          restart();
-          });
-    }
-    } 
-    else {
-      curCard.toggleClass("unmatch show");
-      preCard.toggleClass("unmatch open");
-      setTimeout(function() {
-        curCard.toggleClass("unmatch show");
-        preCard.toggleClass("unmatch show");
-        }, 500);
-      cardSelected=false;
-    }
-    }
+    if((cardSelected==true) && (matched<=8) ){
+      var curCard= $(this).attr("class");
+      var curCardType=$(this).children().attr("class");
+
+    if (curCard === "card") {
+      if (curCardType === preCardType) {
+        $(this).toggleClass("match show");
+        $(".open.show").toggleClass("match show open");
+        cardSelected=false;
+        matched+=1;
+
+      if(matched==8){
+        clearInterval(intervalId);
+        swal({
+          title: "Congratulations! You Won!",
+          text: "With "+moveCount+" moves and "+starsNum+" stars woooo!"  ,
+          type: "success",
+          button: "play again",
+            }).then(function() {
+            restart();
+            });
+        }
+        } 
+      else {
+        $(this).addClass("unmatch show");
+        $(".open.show").addClass("unmatch show").removeClass("open");
+        setTimeout(function() {
+          $(".unmatch.show").removeClass("unmatch show");
+          $(".open.show").removeClass("unmatch show ");
+          }, 500);
+        cardSelected=false;
+      }
+      }}
+
     else{
-      preCard = $(this);
-      preCardType=preCard.children().attr("class");
-      preCard.toggleClass("show open");
+      preCard = $(this).attr("class");
+      preCardType=$(this).children().attr("class");
+      $(this).addClass("show open ");
       cardSelected=true;
     }
-    if(moveCount>= 18){
+    if(moveCount>= 18 && moveCount<22){
       $("#3").remove();
-      starsNum-=1;
+      starsNum=2;
     }
-    if(moveCount>= 22){
+    else if(moveCount>= 22){
       $("#2").remove();
-      starsNum-=1;
+      starsNum=1;
     }
   });
+
 }
 //function of restart
 document.getElementById("restart").addEventListener("click", restart);
@@ -97,10 +104,10 @@ function restart (){
     for (var i = 0; i < cards.length; i++) {
     $(cards[i]).removeClass("show open match");
   }}, 500);
-  location.reload();
+  clearTimeout(intervalId);
+  document.getElementById("time").textContent ="00:00";
+  startGame();
 }
-//start game when loud the page
-window.onload = startGame();
 
 //functions of timer
 var time, intervalId;
@@ -116,7 +123,6 @@ function incrementTime() {
     ":" + ("0" + (time % 60)).slice(-2);
 }
 
-
-
-
+//start game when loud the page
+window.onload = startGame();
 
